db4746e614d33f6c26d95630b0d40e2b6f58a481
Anhelina Martyniuk <anmarty@softserveinc.com>
2024-10-23 22:53:04 +0200

    basic configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Model, Survey} from "survey-react-ui";
+import {surveyJson} from "./model";
+import 'survey-core/defaultV2.min.css';
+import {DoubleBorderLight} from "survey-core/themes"
+import {Typography} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const survey = new Model(surveyJson);
+    survey.applyTheme(DoubleBorderLight);
+
+    return (
+        <>
+            <Typography align={'center'} style={{margin: 'auto'}} variant={'h4'}>Patient Registration</Typography>
+            <Survey model={survey}/>;
+        </>
+    )
 }
 
 export default App;
---
+++ src/model.ts
@@ -0,0 +1,11 @@
+export const surveyJson = {
+    elements: [{
+        name: "FirstName",
+        title: "Enter your first name:",
+        type: "text"
+    }, {
+        name: "LastName",
+        title: "Enter your last name:",
+        type: "text"
+    }]
+};
